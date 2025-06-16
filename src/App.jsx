97b7bc1d6b5994d234d2f4d008c326476cba9f14
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2025-06-16 16:49:40 +0500

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
 import "./App.css";
-
+import logo from "./assets/Group 20.svg";
+import search from "./assets/Vector (1).svg";
+import phone from "./assets/phone.svg";
 function App() {
   return (
     <>
       <div>
-        <h1>Salom Ibrohim</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-          quisquam quis, tenetur repellendus consectetur sequi voluptate neque.
-          Aperiam quae itaque iste, deserunt cumque, nemo, ipsum labore soluta
-          facere error natus.
-        </p>
+        <header>
+          <div className="container">
+            <nav>
+              <img src={logo} alt="" />
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">
+                    <select name="" id="">
+                      <option value="">Menu</option>
+                      <option value="">Drink</option>
+                      <option value="">Food</option>
+                    </select>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    {" "}
+                    <select name="" id="">
+                      <option value="">Services</option>
+                      <option value="">Drink</option>
+                      <option value="">Food</option>
+                    </select>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Offers</a>
+                </li>
+              </ul>
+              <div className="right">
+                <img src={search} alt="" />
+                <button>
+                  <img src={phone} alt="" />
+                  Contact
+                </button>
+              </div>
+            </nav>
+          </div>
+        </header>
+        <main></main>
+        <footer></footer>
       </div>
     </>
   );
